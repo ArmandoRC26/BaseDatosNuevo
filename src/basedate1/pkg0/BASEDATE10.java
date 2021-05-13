993802b15866fc7fc82a230c454a56e56f20e3b3
Armando Rosado <62960138+ArmandoRC26@users.noreply.github.com>
2021-05-13 16:01:49 -0500

    codigo fuente

---
+++ src/basedate1/pkg0/BASEDATE10.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package basedate1.pkg0;
+
+/**
+ *
+ * @author arman
+ */
+public class BASEDATE10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        Inicio1 c=new Inicio1();
+        c.setVisible(true);
+    }
+    
+}
